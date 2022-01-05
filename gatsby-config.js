17351662fa95718e50de61dbfa7bbb8db311c998
michelebruno <43304601+michelebruno@users.noreply.github.com>
2022-01-05 16:21:33 +0100

    Seo and svg loadere

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,39 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby Eslint Starter",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: `${process.env.NODE_ENV !== 'production' ? '[DEV] ' : ''}Gatsby Eslint Starter`,
+    titleTemplate: `${process.env.NODE_ENV !== 'production' ? '[DEV]' : ''}%s - Michele Bruno`,
+    description: 'A new awesome website',
+    image: '/ident-bumper.jpg', // Path to your image you placed in the 'static' folder
   },
   plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
-        icon: "src/images/icon.png",
+        rule: {
+          include: /svg[\\/](.*)\.svg$/, // It should match all svg in svg folder
+        },
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "images",
-        path: "./src/images/",
+        icon: 'src/images/icon.png',
       },
-      __key: "images",
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
     },
   ],
 };
